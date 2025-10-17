d4e5165dbb15db82d8a520423f5407b42b67ae3c
Mark0-20 <marcoahm2005@gmail.com>
2025-10-16 20:54:26 -0600

    Se agregaron las dependencias que se van a utilizar en el proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,19 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    // --- Networking: Retrofit + Moshi ---
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+
+// --- Corrutinas para llamadas asíncronas ---
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+// --- Carga de imágenes: Coil Compose ---
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
+// --- Navegación en Jetpack Compose ---
+    implementation("androidx.navigation:navigation-compose:2.8.0")
 }
